9cbece016e9dc15eb8e9be7c1a1c81037f7bebee
shababrc <chowdhuryshababrob@gmail.com>
2023-04-18 07:39:02 -0700

    set up connection, index functions partially complete

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,26 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'password123',
+        database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+);
 
 
 
--- index.js
+++ index.js
@@ -1,14 +1,96 @@
-const inquirer = require('inquirer');
-require('console.table');
 // will need to bring in connection to db
+//as well as various other inports
+//express needed to run the app
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password123',
+    database: 'company_db'
+  },
+  console.log(`Connected to the company_db database.`)
+);
+
+function viewAllEmployees(){
+    // make a request to the db to select * from employee
+    //when you get the results you will console.table them to display
+    //after the results are displayed you will want to recall mainQuestion()
+    db.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainQuestion();
+    });
+}
+
+function viewAllDepartments(){
+    // make a request to the db to select * from department
+    //when you get the results you will console.table them to display
+    //after the results are displayed you will want to recall mainQuestion()
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainQuestion();
+    });
+}
+
+function viewAllRoles(){
+    // make a request to the db to select * from roles
+    //when you get the results you will console.table them to display
+    //after the results are displayed you will want to recall mainQuestion()
+    db.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainQuestion();
+    });
+}
+function addDepartment(newDepartment){
+    //make a request to the db to insert into department
+    //when you get the results you will console.table them to display
+    //after the results are displayed you will want to recall mainQuestion()
+    db.query(`INSERT INTO department SET?`, newDepartment, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainQuestion();
+    });
+}
+
+function addRole(){
+    // make a request to the db to insert into roles
+    //console.table the results to display them to the console
+    //then recall mainQuestion()
+}
+
+function addEmployee(){
+    //make a request to the db to insert into employee
+    //console.table the results to display them to the console
+    //then recall mainQuestion()
+
+}
+
 
 function mainQuestion(){
-    inquirer.prompt({
+    //set up the main question in inquirer, asking the user what they would like to do,
+    //then it will call the appropriate function based on what the user selected
+        inquirer.prompt({
         type: "list",
         name: "mainQuestion",
         message: "What would you like to do?",
         choices: ["View all employees", "View all departments",
-                "View all roles", "Im done."]
+                "View all roles", "I'm done."]
     })
     .then(({mainQuestion})=>{
         switch(mainQuestion){
@@ -29,12 +111,6 @@ function mainQuestion(){
         }
 
     }) 
-}
-
-function viewAllEmployees(){
-    // make a request to the db to select * from employee
-    //when you get the results you will console.table them to display
-    //after the results are displayed you will want to recall mainQuestion()
 }
 
 function init(){
